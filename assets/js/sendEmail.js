1adc73c01b85ac7e2aa6380f8090350f2e32f53a
pirzer <pirzer1@yahoo.com>
2022-11-24 23:00:25 +0000

    document getElementById contactform reset sendemails line 10

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -2,11 +2,12 @@ function sendMail(contactForm) {
     emailjs.send("service_907mv6g", "rosie", {
         "from_name": contactForm.name.value,
         "from_email": contactForm.emailaddress.value,
-        "project_request": contactForm.projectsummary.value
+        "message": contactForm.projectsummary.value
     })
     .then(
         function(response) {
             console.log("SUCCESS", response);
+            document.getElementById("contactform").reset();
         },
         function(error) {
             console.log("FAILED", error);
